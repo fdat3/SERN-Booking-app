99f10155b1acdfd57c673df99dff0ffe7dcf0923
fdat3 <datp0628@gmail.com>
2023-03-19 17:33:52 +0700

    connectDB and render data to view

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,14 @@
-let getHomePage = (req, res) => {
-    return res.render("home.ejs")
+import db from "../models/index"
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log('---------------')
+        console.log(data)
+        return res.render("home.ejs", { data: JSON.stringify(data) })
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 
