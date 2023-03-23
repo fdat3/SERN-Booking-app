f7b9d4a16c969e0ab6e009c58b8ef5a9090c880e
fdat3 <datp0628@gmail.com>
2023-03-23 15:25:38 +0700

    --UPDATE CORS--
    Install cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import cors from 'cors'
 require('dotenv').config();
 
 let app = express();
+app.use(cors({
+    origin: true
+}))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(
