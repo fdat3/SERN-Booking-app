cc0880b44bfa8fccff59d7de68aa5bcae35276b4
fdat3 <datp0628@gmail.com>
2023-03-27 15:59:59 +0700

    --fix--

--- src/routes/web.js
+++ src/routes/web.js
@@ -14,13 +14,13 @@ let initWebRoutes = (app) => {
     router.get('/display-crud', homeController.displayCRUD)
     router.get('/edit-crud', homeController.editCRUD)
     router.get('/delete-crud', homeController.deleteCRUD)
-    router.get('/api/get-all-user', userController.handleGetAllUser)
-
     router.post('/update-crud', homeController.updateCRUD)
     router.post('/post-crud', homeController.postCRUD)
+    // API
     router.post('/api/login', userController.handleLogin)
+    router.get('/api/get-all-user', userController.handleGetAllUser)
     router.post('/api/create-new-user', userController.handleCreateNewUser)
-    router.post('/api/update-user', userController.handleUpdateUser)
+    router.put('/api/update-user', userController.handleUpdateUser)
     router.get('/api/delete-user', userController.handleDeleteUser)
 
 
